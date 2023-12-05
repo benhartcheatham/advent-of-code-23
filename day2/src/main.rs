a81da3c27e39b3d3b9bb37e06db1fb7ff3590b41
Ben Cheatham <benhartcheatham@gmail.com>
2023-12-04 21:41:51 -0600

    Minor clippy and rustfmt cleanups
    Also use .lines() instead of splitting on newlines. (found this after
    the fact)
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,5 @@
-use std::{fs, io};
 use std::collections::HashMap;
+use std::{fs, io};
 
 fn main() -> Result<(), io::Error> {
     let input = fs::read_to_string("../input.txt")?;
@@ -8,31 +8,31 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn parse_pull<'a>(pull: &'a str) -> Vec<(&'a str, u32)> {
+fn parse_pull(pull: &str) -> Vec<(&str, u32)> {
     let mut cubes = Vec::new();
-    let pull: Vec<&str> = pull.split(",").map(|s| s.trim()).collect();
+    let pull: Vec<&str> = pull.split(',').map(|s| s.trim()).collect();
 
     for c in pull {
-        let temp: Vec<&str> = c.split(" ").collect();
+        let temp: Vec<&str> = c.split(' ').collect();
 
         if temp.len() != 2 {
             continue;
         }
 
-        cubes.push((temp[1], u32::from_str_radix(temp[0], 10).unwrap()));
+        cubes.push((temp[1], temp[0].parse::<u32>().unwrap()));
     }
 
     cubes
 }
 
 fn solution(input: &str) -> u32 {
-    let lines: Vec<&str> = input.split("\n").collect();
+    let lines: Vec<&str> = input.lines().collect();
     let mut sum = 0;
 
     for g in lines.iter() {
         let mut map: HashMap<&str, u32> = HashMap::new();
-        let data = (*g.split(":").collect::<Vec<&str>>().last().unwrap()).trim();
-        let pulls: Vec<&str> = data.split(";").collect();
+        let data = (*g.split(':').collect::<Vec<&str>>().last().unwrap()).trim();
+        let pulls: Vec<&str> = data.split(';').collect();
 
         for p in pulls {
             for c in parse_pull(p) {
@@ -46,5 +46,5 @@ fn solution(input: &str) -> u32 {
         sum += map.into_values().reduce(|acc, e| acc * e).unwrap_or(0);
     }
 
-    sum as u32
+    sum
 }
