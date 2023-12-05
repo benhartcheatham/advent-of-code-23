a60f320bf047c0fbb17b2e2479e3ec927854e709
Ben Cheatham <benhartcheatham@gmail.com>
2023-12-04 21:37:13 -0600

    Day one part two solution
    Had to look up help for this one, was on the right track though :/.
    Final fix ended up being putting the number *inside* the word (e.g.
    "one" became "o1ne") instead of before/after the word.
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,19 +7,38 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
+fn check_line(line: &str) -> String {
+    let words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut ret = line.to_string();
+
+    for (w, d) in words.iter().zip(1..=9) {
+        let match_str = ret.clone();
+
+        for (i, (mi, _)) in match_str.match_indices(w).enumerate() {
+            ret.insert(mi + i + 1, char::from_digit(d, 10).unwrap());
+        }
+    }
+
+    ret
+}
+
 fn solution(input: &str) -> u32 {
     let lines: Vec<&str> = input.split("\n").collect();
     let mut nums = Vec::new();
 
     for c in lines {
-        let digits: Vec<u32> = c.chars().filter(|c| c.is_numeric()).map(|c| c.to_digit(10).unwrap()).collect();
+        let digits: Vec<u32> = check_line(c)
+            .chars()
+            .filter(|c| c.is_numeric())
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
 
         if digits.is_empty() {
             continue;
         }
 
-        let num = digits.first().unwrap() * 10 + digits.last().unwrap();
-        println!("num: {}", num);
         nums.push(digits.first().unwrap() * 10 + digits.last().unwrap());
     }
 
