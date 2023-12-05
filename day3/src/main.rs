acbcd8d22db244e20d77ee2d4357be8aaa819492
Ben Cheatham <benhartcheatham@gmail.com>
2023-12-04 21:48:35 -0600

    Day Three part 2 solution
    Got it on the first try *smirks*
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,109 +7,87 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-/// takes 3 lines (if applicable), and idx should be the
-/// index in the "middle" line that we want to check
-/// lines 0 & 2 can be None, line 1 MUST be Some(&str)
-fn check_line(lines: &[Option<&str>], idx: usize, len: usize) -> bool {
-    if lines.len() != 3 {
-        println!("incorrect number of lines!");
-        return false;
-    }
+fn construct_numbers(line: &str, idx: usize) -> Option<Vec<u32>> {
+    let mut filter = line.to_string();
 
-    let start = if idx == 0 { 0 } else { idx - 1 };
-    let end = if idx == 0 { len + 1 } else { len + 2 };
-    let sub0 = if let Some(s) = lines[0] {
-        Some(s.chars().skip(start).take(end))
-    } else {
-        None
-    };
-    let mut sub1: String = lines[1].unwrap().chars().skip(start).take(end).collect();
-    let sub2 = if let Some(s) = lines[2] {
-        Some(s.chars().skip(start).take(end))
-    } else {
-        None
-    };
-
-    if let Some(mut s) = sub0 {
-        if s.any(|c| c != '.') {
-            return true;
+    let mut left_idx = idx;
+    let left = line.chars().rev().skip(line.len() - left_idx);
+    for c in left {
+        if c.is_numeric() {
+            left_idx -= 1;
+        } else {
+            break;
         }
     }
 
-    if let Some(mut s) = sub2 {
-        if s.any(|c| c != '.') {
-            return true;
+    let mut right_idx = idx + 1;
+    let right = line.chars().skip(right_idx);
+    for c in right {
+        if c.is_numeric() {
+            right_idx += 1;
+        } else {
+            break;
         }
     }
 
-    if end + idx >= lines[1].unwrap().len() {
-        sub1.push('.');
-    } else if idx == 0 {
-        sub1 = String::from(".") + &sub1;
+    filter = filter
+        .chars()
+        .skip(left_idx)
+        .take(right_idx - left_idx)
+        .map(|c| if c.is_numeric() { c } else { ' ' })
+        .collect();
+    let ret: Vec<u32> = filter
+        .split_whitespace()
+        .filter(|s| s.len() > 0)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+
+    if ret.len() > 0 {
+        Some(ret)
+    } else {
+        None
+    }
+}
+
+/// takes the lines around a * and the idx of the * in lines[1]
+fn find_numbers(lines: &[Option<&str>], idx: usize) -> Vec<u32> {
+    let mut nums = Vec::new();
+
+    for line in lines {
+        if let Some(l) = line {
+            let ns = construct_numbers(l, idx);
+
+            if ns.is_some() {
+                for n in ns.unwrap() {
+                    nums.push(n)
+                }
+            }
+        }
     }
 
-    if !sub1.starts_with('.') || !sub1.ends_with('.') {
-        true
-    } else {
-        false
-    }
+    nums
 }
 
 fn solution(input: &str) -> u32 {
     let lines: Vec<&str> = input.lines().collect();
     let mut sum = 0;
 
-    let mut idx: i32 = -1;
-    let mut len = 0;
-    let mut num = String::new();
-
     for i in 0..lines.len() {
         for (j, c) in lines[i].char_indices() {
-            if c.is_numeric() {
-                if idx == -1 {
-                    idx = j as i32;
-                }
-
-                len += 1;
-                num.push(c);
-            } else if idx != -1 {
+            if c == '*' {
                 let l0 = if i == 0 { None } else { Some(lines[i - 1]) };
                 let l2 = if i == lines.len() - 1 {
                     None
                 } else {
                     Some(lines[i + 1])
                 };
-                let l = [l0, Some(lines[i]), l2];
 
-                if check_line(&l, idx as usize, len) {
-                    sum += num.parse::<u32>().unwrap();
+                let nums = find_numbers(&[l0, Some(lines[i]), l2], j);
+                if nums.len() == 2 {
+                    sum += nums[0] * nums[1];
                 }
-
-                idx = -1;
-                len = 0;
-                num = String::new();
             }
         }
-
-        if idx != -1 {
-            let l0 = if i == 0 { None } else { Some(lines[i - 1]) };
-            let l2 = if i == lines.len() - 1 {
-                None
-            } else {
-                Some(lines[i + 1])
-            };
-            let l = [l0, Some(lines[i]), l2];
-
-            if check_line(&l, idx as usize, len) {
-                sum += num.parse::<u32>().unwrap();
-            }
-
-            idx = -1;
-            len = 0;
-            num = String::new();
-        }
-
-        println!("sum: {}\n", sum);
     }
 
     sum
