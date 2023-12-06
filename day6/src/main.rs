5a08e2608fe26c97fd44c4122a5e4eabf9cfd68e
Ben Cheatham <benhartcheatham@gmail.com>
2023-12-06 11:15:53 -0600

    Day six part 2 solution
    First try *smirks*
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 fn main() -> Result<(), io::Error> {
     let input = include_str!("../../input.txt");
 
-    println!("solution: {}", solution(&input));
+    println!("solution: {}", solution(input));
     Ok(())
 }
 
-fn process_race(i: u32, race: (u32, u32)) -> Option<u32> {
+fn process_race(i: u64, race: (u64, u64)) -> Option<u64> {
     if i * (race.0 - i) > race.1 {
         Some(i)
     } else {
@@ -15,28 +15,32 @@ fn process_race(i: u32, race: (u32, u32)) -> Option<u32> {
     }
 }
 
-fn solution(input: &str) -> u32 {
+fn solution(input: &str) -> u64 {
     let lines: Vec<&str> = input.lines().collect();
 
     if lines.len() != 2 {
         panic!("Invalid Input!");
     }
 
-    let times: Vec<u32> = lines[0].split_whitespace().skip(1).map(|n| n.parse::<u32>().unwrap()).collect();
-    let distances: Vec<u32> = lines[1].split_whitespace().skip(1).map(|n| n.parse::<u32>().unwrap()).collect();
+    let time: u64 = lines[0]
+        .split(':')
+        .skip(1)
+        .map(|n| n.replace(' ', "").parse::<u64>().unwrap())
+        .next()
+        .unwrap();
+    let distance: u64 = lines[1]
+        .split(':')
+        .skip(1)
+        .map(|n| n.replace(' ', "").parse::<u64>().unwrap())
+        .next()
+        .unwrap();
 
-    let mut sum = 1;
-    for (t, d) in times.iter().zip(distances) {
-        let t = *t;
+    let (t, d) = (time, distance);
+    let s = (0..t).find_map(|i| process_race(i, (t, d)));
+    let e = (0..t).rev().find_map(|i| process_race(i, (t, d)));
 
-        let s = (0..t).find_map(|i| process_race(i, (t, d)));
-        let e = (0..t).rev().find_map(|i| process_race(i, (t, d)));
-
-        match (s, e) {
-            (Some(v0), Some(v1)) => sum *= v1 - v0 + 1,
-            _ => continue,
-        }
+    match (s, e) {
+        (Some(v0), Some(v1)) => v1 - v0 + 1,
+        _ => 0,
     }
-
-    sum
 }
