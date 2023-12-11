6aa21fc66134754afb655d1e5bb6cc1aff6efaa3
Ben Cheatham <benhartcheatham@gmail.com>
2023-12-11 10:03:50 -0600

    Day eleven part 1 solution
    First try *smirks*
    
    (I did look at reddit beforehand, I would have done a graph otherwise
    and really hated life in part 2)
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

---
+++ day11/src/main.rs
@@ -0,0 +1,94 @@
+use std::collections::HashSet;
+use std::fmt::Display;
+use std::io;
+
+#[derive(Debug)]
+#[allow(unused)]
+struct Galaxy {
+    // for debugging
+    id: usize,
+    coords: (usize, usize),
+}
+
+impl Galaxy {
+    fn new(id: usize, coords: (usize, usize)) -> Self {
+        Galaxy { id, coords }
+    }
+
+    fn distance(&self, other: &Galaxy) -> u64 {
+        let (sr, sc) = (self.coords.0 as i64, self.coords.1 as i64);
+        let (or, oc) = (other.coords.0 as i64, other.coords.1 as i64);
+
+        let a = or - sr;
+        let b = oc - sc;
+
+        (a.abs() + b.abs()) as u64
+    }
+}
+
+impl Display for Galaxy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {:?}", self.id, self.coords)
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    let input = include_str!("../../input.txt");
+
+    println!("solution: {}", solution(input));
+    Ok(())
+}
+
+fn solution(input: &str) -> u64 {
+    let lines = input.lines();
+    let mut space: Vec<Galaxy> = Vec::new();
+    let mut expand_rows: Vec<usize> = Vec::new();
+    let mut expand_columns: HashSet<usize> = HashSet::new();
+    let mut sum = 0;
+
+    let mut id = 1;
+    for (i, line) in lines.enumerate() {
+        let first_id = id;
+
+        for (j, c) in line.char_indices() {
+            if c == '#' {
+                space.push(Galaxy::new(id, (i, j)));
+                expand_columns.insert(j);
+                id += 1;
+            }
+        }
+
+        if id == first_id {
+            expand_rows.push(i);
+        }
+    }
+
+    let expand_columns: Vec<usize> = (0..input.lines().next().unwrap().len())
+        .filter(|i| !expand_columns.contains(i))
+        .collect();
+
+    for (i, r) in expand_rows.iter().enumerate() {
+        for g in space.iter_mut() {
+            if g.coords.0 > (r + i) {
+                g.coords.0 += 1;
+            }
+        }
+    }
+
+    for (j, c) in expand_columns.iter().enumerate() {
+        for g in space.iter_mut() {
+            if g.coords.1 > (c + j) {
+                g.coords.1 += 1;
+            }
+        }
+    }
+
+    for i in 0..(space.len() - 1) {
+        for j in (i + 1)..space.len() {
+            let distance = space[i].distance(&space[j]);
+            sum += distance;
+        }
+    }
+
+    sum
+}
