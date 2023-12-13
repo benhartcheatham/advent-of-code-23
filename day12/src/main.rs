17a8554741bd4858a1269d4617e5624f407c5a66
Ben Cheatham <benhartcheatham@gmail.com>
2023-12-13 11:57:42 -0600

    Day twelve part 2 solution
    Wasn't able to do this myself. I did know what I needed to do for a
    top-down dynamic programming solution, I just suck at dynamic
    programming and couldn't get it to work. I ended up taking a bottom-up
    solution from reddit (linked in comment above function) and annotating
    it as a next-best-thing. Oh well.
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -18,26 +18,72 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn find_arrangements(springs: String, groups: &Vec<usize>) -> u32 {
-    if !springs.contains('?') {
-        let hashes: Vec<_> = springs.split('.').filter(|h| !h.is_empty()).collect();
+/*
+ * from here: https://www.reddit.com/r/adventofcode/comments/18ge41g/2023_day_12_solutions/kd18cl9/
+ * comments by me :)
+ */
+fn find_arrangements(springs: &str, groups: &Vec<bool>) -> u64 {
+    let mut dp: Vec<Vec<u64>> = Vec::new();
+    // dp[i][j] == [character c][possible states of c]
+    dp.resize(springs.len() + 1, Vec::new());
 
-        if hashes.len() != groups.len() {
-            return 0;
-        }
+    for r in dp.iter_mut() {
+        r.resize(groups.len() + 1, 0);
+    }
 
-        if hashes.iter().enumerate().all(|(i, h)| h.len() == groups[i]) {
-            return 1;
-        } else {
-            return 0;
+    // "base" case
+    dp[springs.len()][groups.len()] = 1;
+
+    // compare spring ('#', '.', or '?') to group[j] ('T', 'F")
+    for (i, c) in springs.char_indices().rev() {
+        for j in (0..groups.len()).rev() {
+            let (mut damaged, mut operational) = (false, false);
+
+            // set what possible state the character could have
+            match c {
+                '#' => damaged = true,
+                '.' => operational = true,
+                _ => {
+                    damaged = true;
+                    operational = true
+                }
+            }
+
+            let mut sum = 0;
+
+            // if this character could be damaged then we must be in a group
+            if damaged && groups[j] {
+                sum += dp[i + 1][j + 1];
+            // if this character is operational then we must be in a gap of
+            // operational springs
+            } else if operational && !groups[j] {
+                sum += dp[i + 1][j + 1] + dp[i + 1][j];
+            }
+
+            // this character is the sum of possibilites based on the
+            // 1-2 characters before it
+            dp[i][j] = sum;
         }
     }
 
-    find_arrangements(springs.replacen('?', "#", 1), groups)
-        + find_arrangements(springs.replacen('?', ".", 1), groups)
+    dp[0][0]
 }
 
-fn solution(input: &str) -> u32 {
+fn make_groups(groups: Vec<usize>) -> Vec<bool> {
+    let mut bools = Vec::new();
+
+    bools.push(false);
+
+    for g in groups {
+        bools.append(&mut vec![true; g]);
+
+        bools.push(false);
+    }
+
+    bools
+}
+
+fn solution(input: &str) -> u64 {
     let lines = input.lines();
     let mut sum = 0;
 
@@ -48,14 +94,17 @@ fn solution(input: &str) -> u32 {
             continue;
         }
 
-        let springs = parts[0];
-        let groups: Vec<_> = parts[1]
+        let mut springs = (parts[0].to_string() + "?").repeat(5);
+        springs.pop();
+        springs = ".".to_string() + &springs + ".";
+        let groups: Vec<_> = (parts[1].to_string() + ",")
+            .repeat(5)
             .split(',')
+            .filter(|s| !s.is_empty())
             .map(|n| n.parse::<usize>().unwrap())
             .collect();
 
-        println!("finding arrangements for spring {}!", springs);
-        sum += find_arrangements(springs.to_string(), &groups);
+        sum += find_arrangements(&springs.to_string(), &make_groups(groups));
     }
 
     sum
