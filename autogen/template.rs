c0f036b52e33960fd7b027a741fe537a722af079
Ben Cheatham <Benjamin.Cheatham@amd.com>
2023-12-19 14:11:21 -0600

    Add autogen directory
    "autogen" directory will be for tooling for advent-of-code. Move
    create-day script and template.rs into autogen, and add a pre-commit git
    hook for anyone that wants to use it.
    
    Signed-off-by: Ben Cheatham <Benjamin.Cheatham@amd.com>

---
+++ autogen/template.rs
@@ -0,0 +1,23 @@
+use clap::{arg, command, ArgAction};
+use std::io;
+
+fn main() -> Result<(), io::Error> {
+    let input = include_str!("../../input.txt");
+    let example = include_str!("../../example.txt");
+
+    let matches = command!()
+        .arg(arg!(example: -e).action(ArgAction::SetTrue))
+        .get_matches();
+
+    if matches.get_flag("example") {
+        println!("solution (example): {}", solution(example));
+    } else {
+        println!("solution: {}", solution(input));
+    }
+
+    Ok(())
+}
+
+fn solution(input: &str) -> u64 {
+    0
+}
