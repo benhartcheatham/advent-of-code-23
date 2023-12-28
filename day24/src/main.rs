eccdb5b61382240d15c052ae635b16aa3eb161a3
Ben Cheatham <Benjamin.Cheatham@amd.com>
2023-12-28 10:59:05 -0600

    Day twenty four part 2 solution
    First try (technically).
    
    Looked up a solution to this one on reddit (post and code here:
    https://www.reddit.com/r/adventofcode/comments/18pnycy/comment/kepu26z/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)
    since I didn't want to spend the next 3 days doing math for a
    programming puzzle. The solution is intuitive, but deriving the formulas
    is not easy, so I just copied that person's code.
    
    Signed-off-by: Ben Cheatham <Benjamin.Cheatham@amd.com>

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,43 +1,23 @@
 use clap::{arg, command, ArgAction};
-use nalgebra::{matrix, ArrayStorage, Const, Matrix};
+use nalgebra::{matrix, vector, Matrix3, Matrix6, Vector3};
 use std::io;
-
-type Matrix2d = Matrix<f64, Const<2>, Const<2>, ArrayStorage<f64, 2, 2>>;
-type Vec2d = Matrix<f64, Const<2>, Const<1>, ArrayStorage<f64, 2, 1>>;
 
 #[derive(Debug)]
 struct Hailstone {
-    pos: Vec<f64>,
-    vel: Vec<f64>,
+    pos: Vector3<f64>,
+    vel: Vector3<f64>,
 }
 
-#[allow(unused)]
 impl Hailstone {
-    fn new(x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64) -> Self {
-        Self {
-            pos: vec![x, y, z],
-            vel: vec![vx, vy, vz],
-        }
-    }
-
     fn from_vecs(pos: &[f64], vel: &[f64]) -> Option<Self> {
         if pos.len() != 3 || vel.len() != 3 {
             return None;
         }
 
         Some(Self {
-            pos: pos.to_vec(),
-            vel: vel.to_vec(),
+            pos: vector![pos[0], pos[1], pos[2]],
+            vel: vector![vel[0], vel[1], vel[2]],
         })
-    }
-
-    fn in_bounds(&self, lower: f64, upper: f64, t: f64) -> bool {
-        let (x, y) = (
-            self.pos[0] + (self.vel[0] * t),
-            self.pos[1] + (self.vel[1] * t),
-        );
-
-        (x - lower > 1e-10 && upper - x > -1e-10) && (y - lower > 1e-10 && upper - y > -1e-10)
     }
 }
 
@@ -50,50 +30,48 @@ fn main() -> Result<(), io::Error> {
         .get_matches();
 
     if matches.get_flag("example") {
-        println!("solution (example): {}", solution(7, 27, example));
+        println!("solution (example): {}", solution(example));
     } else {
-        println!(
-            "solution: {}",
-            solution(200_000_000_000_000, 400_000_000_000_000, input)
-        );
+        println!("solution: {}", solution(input));
     }
 
     Ok(())
 }
 
-fn get_2d_matrix(stone0: &Hailstone, stone1: &Hailstone) -> (Matrix2d, Vec2d) {
-    let b = matrix![stone1.pos[0] - stone0.pos[0]; stone1.pos[1] - stone0.pos[1]];
-    let m = matrix![stone0.vel[0], -stone1.vel[0]; stone0.vel[1], -stone1.vel[1]];
-
-    (m, b)
+fn skew_matrix(v: &Vector3<f64>) -> Matrix3<f64> {
+    matrix![0.0, -v[2], v[1];
+            v[2], 0.0, -v[0];
+            -v[1], v[0], 0.0]
 }
 
-fn find_solutions(lower: f64, upper: f64, stones: &[Hailstone]) -> u64 {
+fn find_solution(stones: &[Hailstone]) -> u64 {
+    let v0 = -stones[0].pos.cross(&stones[0].vel) + stones[1].pos.cross(&stones[1].vel);
+    let v1 = -stones[0].pos.cross(&stones[0].vel) + stones[2].pos.cross(&stones[2].vel);
+    let rhs = matrix![v0[0]; v0[1]; v0[2]; v1[0]; v1[1]; v1[2]];
+
+    let m00 = skew_matrix(&stones[0].vel) - skew_matrix(&stones[1].vel);
+    let m30 = skew_matrix(&stones[0].vel) - skew_matrix(&stones[2].vel);
+    let m03 = -skew_matrix(&stones[0].pos) + skew_matrix(&stones[1].pos);
+    let m33 = -skew_matrix(&stones[0].pos) + skew_matrix(&stones[2].pos);
+    let m: Matrix6<f64> = matrix![ m00[(0, 0)], m00[(0, 1)], m00[(0, 2)], m03[(0, 0)], m03[(0, 1)], m03[(0, 2)];
+       m00[(1, 0)], m00[(1, 1)], m00[(1, 2)], m03[(1, 0)], m03[(1, 1)], m03[(1, 2)];
+       m00[(2, 0)], m00[(2, 1)], m00[(2, 2)], m03[(2, 0)], m03[(2, 1)], m03[(2, 2)];
+       m30[(0, 0)], m30[(0, 1)], m30[(0, 2)], m33[(0, 0)], m33[(0, 1)], m33[(0, 2)];
+       m30[(1, 0)], m30[(1, 1)], m30[(1, 2)], m33[(1, 0)], m33[(1, 1)], m33[(1, 2)];
+       m30[(2, 0)], m30[(2, 1)], m30[(2, 2)], m33[(2, 0)], m33[(2, 1)], m33[(2, 2)];
+    ];
+
+    let res = m.try_inverse().unwrap() * rhs;
+
     let mut sum = 0;
-
-    for i in 0..(stones.len() - 1) {
-        for j in (i + 1)..stones.len() {
-            let (m, b) = get_2d_matrix(&stones[i], &stones[j]);
-            let lu = m.full_piv_lu();
-
-            let Some(ts) = lu.solve(&b) else {
-                continue;
-            };
-
-            if ts[(0, 0)] < 0.0 || ts[(1, 0)] < 0.0 {
-                continue;
-            }
-
-            if stones[i].in_bounds(lower, upper, ts[(0, 0)]) {
-                sum += 1;
-            }
-        }
+    for i in 0..3 {
+        sum += res[(i, 0)].round() as u64;
     }
 
     sum
 }
 
-fn solution(lower: u64, upper: u64, input: &str) -> u64 {
+fn solution(input: &str) -> u64 {
     let lines: Vec<_> = input.lines().collect();
     let mut stones = Vec::new();
 
@@ -119,5 +97,5 @@ fn solution(lower: u64, upper: u64, input: &str) -> u64 {
         stones.push(Hailstone::from_vecs(&pvec, &vvec).unwrap());
     }
 
-    find_solutions(lower as f64, upper as f64, &stones)
+    find_solution(&stones)
 }
